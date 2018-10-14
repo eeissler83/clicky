ea7afae0f0507561cc7df80308a89021a4b00d5c
Eric Eissler <ericeissler@Erics-MacBook-Pro.local>
2018-10-14 00:09:07 -0500

    added json and interaction

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -2,18 +2,87 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/navbar";
 import Jumbotron from "./components/jumbotron";
-// import card from "./components/card";
+import Card from "./components/card";
 import Footer from "./components/footer";
 
-const App = () => (
-<div>
-  <Navbar />
-  <Jumbotron backgroundimage="https://assets1.ignimgs.com/thumbs/userUploaded/2018/8/21/8o4mojba-1534896773944.jpeg" />
-  <Footer />
-</div>
+
+//sets state to 0 or empty
+class App extends Component {
+  state = {
+    cops,
+    clickedCops: [],
+    score: 0
+  };
+
+//when you click on a card ... the fish is taken out of the array
+  imageClick = event => {
+    const currentCops = event.target.alt;
+    const CopsAlreadyClicked =
+      this.state.clickedCops.indexOf(currentCops) > -1;
+
+//if you click on a fish that has already been selected, the game is reset and cards reordered
+    if (CopsAlreadyClicked) {
+      this.setState({
+        cops: this.state.cops.sort(function(a, b) {
+          return 0.5 - Math.random();
+        }),
+        clickedCops: [],
+        score: 0
+      });
+        alert("You lose. Play again?");
+
+//if you click on an available fish, your score is increased and cards reordered
+    } else {
+      this.setState(
+        {
+          cops: this.state.cops.sort(function(a, b) {
+            return 0.5 - Math.random();
+          }),
+          clickedCops: this.state.clickedCops.concat(
+            currentCops
+          ),
+          score: this.state.score + 1
+        },
+//if you get all 12 fish corrent you get a congrats message and the game resets        
+        () => {
+          if (this.state.score === 12) {
+            alert("Yay! You Win!");
+            this.setState({
+              cops: this.state.cops.sort(function(a, b) {
+                return 0.5 - Math.random();
+              }),
+              clickedCops: [],
+              score: 0
+            });
+          }
+        }
+      );
+    }
+  };
 
 
-);
 
-
+//the order of components to be rendered: navbar, jumbotron, friendcard, footer 
+render() {
+  return (
+    <div>
+      <Navbar 
+        score={this.state.score}
+      />
+      <Jumbotron />
+      <div className="wrapper">
+        {this.state.cops.map(cops => (
+          <Card
+            imageClick={this.imageClick}
+            id={cops.id}
+            key={cops.id}
+            image={cops.image}
+          />
+        ))}
+      </div>
+      <Footer />
+    </div>
+  );
+}
+}
 export default App;
