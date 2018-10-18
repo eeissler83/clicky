eb385c15281bab8ba87f33b0fb4774936524744e
Eric Eissler <ericeissler@Erics-MacBook-Pro.local>
2018-10-17 22:34:22 -0500

    yarn?

--- clicky/src/components/jumbotron/jumbotron.js
+++ clicky/src/components/jumbotron/jumbotron.js
@@ -5,7 +5,7 @@ import "./jumbotron.css";
 const jumbotronStyle = {
   paddingBottom: '150px',
   boxShadow: "0px 0px 0px 0px rgba(0,0,0,0)",
-  height: "300px"
+  height: "240px"
 
 }
 
@@ -13,7 +13,7 @@ const Jumbotron = () =>
       <div className="card-panel grey lighten-2" style={jumbotronStyle}>
         <div className="container">
           <h1>Paradise PD</h1>
-          <h2>Clicky Game!</h2>
+          {/* <h2>Memory Game</h2> */}
           <h5>Click on an image to earn points, but don't click on any more than once!</h5>
         </div>
        </div>
